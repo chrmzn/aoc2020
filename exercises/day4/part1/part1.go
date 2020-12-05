520638fb8d2c81f7f0bed6b5f34ae18ef52bdc1c
Christopher O'Brien <chris@theobriens.co.nz>
2020-12-06 08:42:47 +1300

    add file read template for day 4 part 1

---
+++ exercises/day4/part1/part1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Day 4 - Part 1")
+
+	readFile, err := os.Open("../input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	var fileTextLines []string
+
+	for fileScanner.Scan() {
+		fileTextLines = append(fileTextLines, fileScanner.Text())
+	}
+
+	readFile.Close()
+
+	for _, line := range fileTextLines {
+		fmt.Println(line)
+		if line == "" {
+			fmt.Println("New Person!")
+		}
+	}
+}
