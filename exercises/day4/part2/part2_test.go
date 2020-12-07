8fd3ceab9029e1d11879397c4c6a41d38f7c797e
Christopher O'Brien <chris@theobriens.co.nz>
2020-12-07 13:57:21 +1300

    add test cases

--- exercises/day4/part2/part2_test.go
+++ exercises/day4/part2/part2_test.go
@@ -3,6 +3,27 @@ package main
 import "testing"
 
 // TestBirthYear Do a thing
-func TestBirthYear(t *testing.T) {
-	t.Errorf("Fail")
+func TestParseBirthYear(t *testing.T) {
+	// PASS
+	_, err := parseBirthYear("1920")
+	if err != nil {
+		t.Errorf("1920 should work")
+	}
+	_, err = parseBirthYear("2002")
+	if err != nil {
+		t.Errorf("2002 should work")
+	}
+	// FAILS
+	_, err = parseBirthYear("1919")
+	if err == nil {
+		t.Errorf("1919 should fail")
+	}
+	_, err = parseBirthYear("2003")
+	if err == nil {
+		t.Errorf("2003 should fail")
+	}
+	_, err = parseBirthYear("abcd")
+	if err == nil {
+		t.Errorf("abcd should fail")
+	}
 }
