67be10c92e32173e9f46ff22d53acf7dec60f88b
Christopher O'Brien <chris@theobriens.co.nz>
2020-12-08 13:11:15 +1300

    day 5 setup

---
+++ exercises/day5/part1/part1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Day 4 - Part 1")
+
+	readFile, err := os.Open("../input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	var fileTextLines []string
+
+	for fileScanner.Scan() {
+		fileTextLines = append(fileTextLines, fileScanner.Text())
+	}
+
+	readFile.Close()
+}
